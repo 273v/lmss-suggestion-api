99baaf515cf8fe1f53692f9d6d478a298488b8b4
Michael Bommarito <michael@bommaritollc.com>
2023-03-17 10:42:50 -0400

    quality of life enhancements for dev docker build/run

--- docker/build.sh
+++ docker/build.sh
@@ -4,5 +4,13 @@
 # Copyright (C) 2023 273 Ventures LLC (https://273ventures.com)
 # SPDX-License-Identifier: MIT
 
+# check if we have an .env.json set and copy from template if not
+if [ -f ".env.json" ]; then
+  echo "Found .env.json file.  Using it to set environment variables."
+else
+  echo "No .env.json file found.  Creating one from template."
+  cp .env.json.template .env.json
+fi
+
 # build with a hard-coded name since we're not using compose/swarm/k8s or registry here
 docker build -t lmss-suggestion-api -f docker/Dockerfile .
--- docker/run.sh
+++ docker/run.sh
@@ -16,11 +16,21 @@ OPENAI_MODEL_NAME=${OPENAI_MODEL_NAME:-"gpt-3.5-turbo-0301"}
 # if OPENAI_API_KEY key is set, use it via -e
 if [ -z "$OPENAI_API_KEY" ]; then
   echo "No OpenAI API key provided through environment variable or CLI.  Using .docker.env file."
+
+  # check if there's a .docker.env file and setup a default if not
+  if [ -f ".docker.env" ]; then
+    echo "Found .docker.env file.  Using it to set environment variables."
+  else
+    echo "No .docker.env file found.  Creating one."
+    echo "OPENAI_API_KEY=${OPENAI_API_KEY}" > .docker.env
+  fi
+
   docker run --publish 8888:${BIND_PORT} \
     --name lmss-suggestion-api \
     --env-file .docker.env \
     lmss-suggestion-api:latest
 else
+  # run with env vars
   docker run --publish 8888:${BIND_PORT} \
     --name lmss-suggestion-api \
     -e OPENAI_API_KEY=${OPENAI_API_KEY} \
